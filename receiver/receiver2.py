42b4a3ec529dcba2cf49fdb5171c835f5bf4730d
simon <simonhaidar2@icloud.com>
2023-02-21 01:33:50 +0200

    Fixed some messages

--- receiver/receiver2.py
+++ receiver/receiver2.py
@@ -2,10 +2,12 @@ import socket
 import os
 import tqdm
 import json
+import hashlib
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 port = 5000
 s.bind(("localhost", port))
+BUFFER_SIZE = 4096
 
 s.listen(5)  # Now wait for client connection.
 print("Listening...")
@@ -15,30 +17,31 @@ print('Connected to: ', addr)
 done = False
 fileDictBytes = ""
 while not done:
-    data = client.recv(1024).decode()
+    data = client.recv(BUFFER_SIZE).decode()
     if("<END>" in data):
-        print("transfer done.\n")
+        print("Received: fileData")
         done = True
         fileDictBytes += data[:-5]
     else:
-        print("transferring...")
+        print("Receiving: fileData")
         fileDictBytes += data
 
 client.send(b"fileDictTransfer")
 fileDict = json.loads(fileDictBytes)
 
 for file in fileDict.keys():
-    progress = tqdm.tqdm(unit="B", unit_scale=True, unit_divisor=1000, total=int(int(fileDict[file])))
-    
+    print("\nReceiving: " + file)
+    progress = tqdm.tqdm(unit="B", unit_scale=True, unit_divisor=1024, total=int(fileDict[file]))
     done = False
     fileBytes = b""
     while not done:
-        data_buffer = client.recv(1024)
-        progress.update(1024)
+        data_buffer = client.recv(BUFFER_SIZE)
+        progress.update(BUFFER_SIZE)
         if(b"<END>" in data_buffer):
             fileBytes += data_buffer
             client.send("fileTransfer".encode())
             done = True
+            print("Received: " + file)
         else:
             fileBytes += data_buffer
     fileBytes = fileBytes.replace(b"<END>", b"")
@@ -48,7 +51,7 @@ for file in fileDict.keys():
         
 print("Closing connection.")
 client.close()
-
+input("Hit enter")
 # while True:
 #     print(str(client.recv(1024).decode()))
 #     a = input("Enter the msg:")
--- sender/sender2.py
+++ sender/sender2.py
@@ -1,9 +1,11 @@
 import socket
 import os
 import json
+import hashlib
 
 s = socket.socket()
 port = 5000
+BUFFER_SIZE = 4096
 
 s.connect(("localhost", port))
 print("Connected.")
@@ -21,17 +23,18 @@ fileDictBytes = json.dumps(fileDict).encode()
 s.send(fileDictBytes)
 s.send(b"<END>")
 
-recv = s.recv(1024).decode()
+recv = s.recv(BUFFER_SIZE).decode()
 if recv == "fileDictTransfer":
-    print("File data delivered successfully.")
+    print("File data delivered.")
     
     for file in fileDict.keys():
         print("sending: " + file)
         filee = open(file, "rb")
         s.sendall(filee.read())
         s.send(b"<END>")
-        while (s.recv(1024).decode() != "fileTransfer"):
+        while (s.recv(BUFFER_SIZE).decode() != "fileTransfer"):
             print("Waiting for receiver...")
         print("delivered: " + file)
 
 s.close()
+input("Hit enter")
