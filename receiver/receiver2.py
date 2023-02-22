c48aae372f6a09b33a29946889859440d905178c
simon <simonhaidar2@icloud.com>
2023-02-22 03:12:04 +0200

    V1.0

--- receiver/receiver2.py
+++ receiver/receiver2.py
@@ -1,7 +1,46 @@
 import socket
 import json
+import os
+import tqdm
 
-from modules import client, BUFFER_SIZE, getDir
+BUFFER_SIZE = 4096
+
+def getDir(dirDict):
+    
+    for item in dirDict.keys():
+        if(isinstance(dirDict[item], list)):
+            try:
+                os.makedirs(dirDict[item][3])
+            except:
+                print("", end="")
+            
+            print("Receiving: " + item)
+            progress = tqdm.tqdm(unit="B", unit_scale=True, unit_divisor=1024, total=int(dirDict[item][1]))
+            done = False
+            fileBytes = b""
+            while not done:
+                progress.update(BUFFER_SIZE)
+                data_buffer = client.recv(BUFFER_SIZE)
+                if(b"<END>" in data_buffer):
+                    fileBytes += data_buffer
+                    client.send(("fileTransfer:" + item).encode())
+                    done = True
+                else:
+                    fileBytes += data_buffer
+            fileBytes = fileBytes.replace(b"<END>", b"")
+            file = open(dirDict[item][3] + item, "wb")
+            file.write(fileBytes)
+            file.close()
+        else:
+            getDir(dirDict[item])
+            
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+port = 5000
+s.bind(("localhost", port))
+s.listen(5)
+print("Listening...")
+client, addr = s.accept()
+print('Connected to: ', addr)
 
 done = False
 dirDictString = ""
--- sender/sender2.py
+++ sender/sender2.py
@@ -1,12 +1,56 @@
 import json
+import os
+import hashlib
+import socket
 
-from modules import readDir, sendDir, BUFFER_SIZE, s
+BUFFER_SIZE = 4096
+s = socket.socket()
+
+totalFilesNb = 0
+
+def readDir(relPath):
+    dirDict = {}
+    filesList = os.scandir(relPath)
+    totalFilesNb = 0
+    for fileObj in filesList:
+        file = fileObj.name
+        if (file == os.path.basename(__file__) or file == "functions.py" or file == "sender2.py"):
+            continue
+        if (fileObj.is_file()):
+            totalFilesNb += 1
+            fileDict = 0
+            sha256_hash = hashlib.sha256()
+            with open(str(relPath + file),"rb") as f:
+                for byte_block in iter(lambda: f.read(4096),b""):
+                    sha256_hash.update(byte_block)
+            fileDigest = sha256_hash.hexdigest()
+        else:
+            fileDict = readDir(relPath + file + "/")
+            fileDigest = ""
+        thisFile = [file, os.stat(relPath + file).st_size, fileDigest, relPath, fileDict]
+        
+        if (fileObj.is_file()):
+            dirDict[thisFile[0]] = thisFile
+        else:
+            dirDict[thisFile[0]] = fileDict
+
+    return dirDict
+
+def sendDir(dirDict):
+    for file in dirDict.keys():
+        if (isinstance(dirDict[file], list)):
+            filee = open(dirDict[file][3] + file, "rb")
+            print("Sending: " + file)
+            s.sendall(filee.read())
+            s.send(b"<END>")
+            while (s.recv(BUFFER_SIZE).decode() != ("fileTransfer:" + file)):
+                print("Waiting for receiver...")
+        else:
+            sendDir(dirDict[file])
 
 # 0 name, 1 size, 2 digest, 3 relPath, 4 dirlist
 
-
 port = 5000
-
 
 print("Reading files and subdirectories...")
 completeFiles = readDir("./")
