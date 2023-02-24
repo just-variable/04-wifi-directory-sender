125854b1a375fb601914a8a67266ac2e4f79ee7c
simon <simonhaidar2@icloud.com>
2023-02-24 15:48:51 +0200

    Added memory buffer on receiver

--- receiver/receiver2.py
+++ receiver/receiver2.py
@@ -8,7 +8,7 @@ BUFFER_SIZE = 2*1024*1024
 def fixName(str):
     if(len(str) > 33):
         str = str[:30] + "..." 
-    return str + " "*(36-len(str))
+    return str + " "*(36-len(str))  
 
 def getDir(dirDict):
     for itemName in dirDict.keys():
@@ -23,8 +23,11 @@ def getDir(dirDict):
                 file = open(dirDict[itemName][3] + itemName, "wb")
                 done = False
 
+                memSize = 0
+                temp = b""
                 while not done:
                     data_buffer = client.recv(BUFFER_SIZE)
+                    memSize += BUFFER_SIZE
                     if(b"<END>" in data_buffer):
                         progress.update(task1, advance=len(data_buffer)-5)
                         client.send(("fileTransfer:" + itemName).encode())
@@ -33,7 +36,12 @@ def getDir(dirDict):
                         done = True
                     else:
                         progress.update(task1, advance=len(data_buffer))
-                        file.write(data_buffer)
+                        if(memSize >= 15*1024*1024):
+                            file.write(temp)
+                            memSize = 0
+                        else:
+                            temp += data_buffer
+                        
                 file.close()
         else:
             getDir(dirDict[itemName])
